1dc99cb40b848d91eb21d516ffbfbb477bef96a7
intbad <int4bad@gmail.com>
2025-03-15 18:24:31 +0500

    Adaptive menu

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,24 +1,22 @@
-<div>
+<div class="px-5 pt-3">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#"> <img src={{ config("app.url") . "/storage/logo.svg" }} width="32"> {{ config('app.name') }} </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="{{ route('plans'); }}"> Main </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('months'); }}"> Months </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('categories'); }}"> Categories </a>
-                    </li>
-                </ul>
-            </div>
+        <a class="navbar-brand" href="{{ route('plans') }}"> <img src={{ config("app.url") . "/storage/logo.svg" }} width="32"> {{ config('app.name') }} </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="{{ route('plans'); }}"> Main </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('months'); }}"> Months </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('categories'); }}"> Categories </a>
+                </li>
+            </ul>
         </div>
     </nav>
 </div>
--- resources/views/months/create.blade.php
+++ resources/views/months/create.blade.php
@@ -1,6 +1,6 @@
 @include('common/head')
 
-<body>
+<body class="bg-dark text-light">
     @include('menu')
     <div class="row px-5 pt-3">
         <div class="col col-xxl"> </div>
@@ -8,13 +8,13 @@
             <div class="">
                 <h1> Add month </h1>
             </div>
-            <div class="mt-5 p-2 text-gray bg-white rounded shadow">
+            <div class="mt-5 p-2 rounded shadow">
                 <form action="store" method="POST">
                     @csrf
                     <div class="row mb-3">
                         <div class="col-12 col-sm">
                             <label for="month" class="form-label"> Month </label>
-                            <select class="form-control" name="month">
+                            <select class="form-control bg-secondary" name="month">
                                 @foreach ($months as $monthNumber => $month)
                                     <option value="{{ $monthNumber }}" {{ $monthNumber === $predictMonthNumber ? "selected" : "" }}> {{ $month }} </option>
                                 @endforeach
@@ -23,7 +23,7 @@
                         </div>
                         <div class="col-12 col-sm">
                             <label for="year" class="form-label"> Year </label>
-                            <select class="form-control" name="year">
+                            <select class="form-control bg-secondary" name="year">
                                 @foreach ($years as $year)
                                     <option value='{{ $year }}' {{ $year === $currentYear ? "selected" : ""}} > {{ $year }} </option>
                                 @endforeach
@@ -32,7 +32,7 @@
                     </div>
                     <div class="row">
                         <div class="col-12 col-sm-3 mt-1 col-sm text-start">
-                            <a href="" class="w-100 btn btn-secondary"> Cancel </a>
+                            <a href="{{ route('months') }}" class="w-100 btn btn-secondary"> Cancel </a>
                         </div>
                         <div class="col-sm"></div>
                         <div class="col-12 col-sm-3 mt-1 col-sm text-end">
